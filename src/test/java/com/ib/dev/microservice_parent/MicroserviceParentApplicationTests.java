3cb20c34d093398ece59be06c0b947c39e2c3972
stefan <stefan@stefan>
2025-02-16 16:06:29 +0100

    Initial commit

---
+++ src/test/java/com/ib/dev/microservice_parent/MicroserviceParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ib.dev.microservice_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
